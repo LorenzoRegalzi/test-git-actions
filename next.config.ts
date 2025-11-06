19d4c211848a198c6e2bfe4797e602f2ccf3c045
Lorenzo Regalzi <lorenzo.regalzi@gmail.com>
2025-11-06 16:24:49 +0100

    Added Next.js app

---
+++ next.config.ts
@@ -0,0 +1,11 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: { unoptimized: true },
+  basePath: process.env.GITHUB_REPOSITORY
+    ? '/' + process.env.GITHUB_REPOSITORY.split('/')[1]
+    : '',
+};
+
+export default nextConfig;
+
